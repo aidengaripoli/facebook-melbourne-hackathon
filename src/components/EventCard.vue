735cb38718091542062b22eac749f82a4aa206d8
Daniel Milner <danielmmilner@gmail.com>
2019-05-05 02:19:17 +1000

    Never gone give you up

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -4,25 +4,19 @@
     style="padding-left: 12%; padding-right:20%; margin-top: 0.75rem;"
   >
     <div class="column">
-      <p>{{eventStartDateTime}}</p>-
-      <p>{{eventEndDateTime}}</p>
+      <p>{{event.time[0]}}</p>-
+      <p>{{event.time[1]}}</p>
     </div>
     <div class="card column is-10 is-paddingless">
       <div class="card-content is-paddingless">
         <div class="columns is-marginless">
           <div class="column" style="padding: 20px">
-            <h1 class="title is-4">{{eventName}}</h1>
-            <div class="tags" v-for="tag in eventTags" v-bind:key="tag">
-              <span class="tag is-dark">{{tag}}</span>
-            </div>
+            <h1 class="title is-4">{{event.name}}</h1>
             <i>{{eventDesc}}</i>
           </div>
           <div class="column is-paddingless">
             <figure class="image is-square">
-              <img
-                src="http://mannum.org.au/content/images/Haythorpe%20Mannum.jpg"
-                alt="Placeholder image"
-              >
+              <img :src="event.photo" alt="Placeholder image">
             </figure>
           </div>
         </div>
@@ -39,16 +33,12 @@
 }
 </style>
 
-
 <script>
 export default {
   name: "EventCard",
   props: {
-    eventName: String,
-    eventStartDateTime: String,
-    eventEndDateTime: String,
-    eventDesc: String,
-    eventTags: Array
+    event: {},
+    eventDesc: String
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,24 +8,18 @@
       <div v-for="(day, index) in plan" :key="index" style="padding-top: 3%">
         <h1 class="title">Sunday 5th May</h1>
         <EventCard
-          :eventName="day.lunch.name"
+          :event="day.lunch"
           eventDesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets conta"
-          eventStartDateTime="10:00am"
-          eventEndDateTime="12:00pm"
         />
         <i class="fas fa-arrow-down" style="padding: 10px"></i>
         <EventCard
-          :eventName="day.middayevent.name"
+          :event="day.middayevent"
           eventDesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets conta"
-          eventStartDateTime="12:30pm"
-          eventEndDateTime="6:00pm"
         />
         <i class="fas fa-arrow-down"></i>
         <EventCard
-          :eventName="day.dinner.name"
+          :event="day.dinner"
           eventDesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets conta"
-          eventStartDateTime="6:45pm"
-          eventEndDateTime="8:00pm"
         />
       </div>
     </div>
