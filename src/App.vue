774e9ef6b16ac1e6af7a54aea45523c393679168
Angus Hudson <angus.hudsoa@gmail.com>
2019-05-04 20:30:55 +1000

    Remove 'about' page

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <div class="background">
       <div id="nav">
         <router-link to="/">Home</router-link>|
-        <router-link to="/about">About</router-link>|
         <router-link to="/events">Events</router-link>|
         <router-link to="/output">Output</router-link>
       </div>
