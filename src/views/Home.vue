a963423d4ca60fcbcdc897138cab973e4ef58b55
Yingcong Zhu From Mac <yingcongzhu@gmail.com>
2019-05-05 06:27:20 +1000

    add send email button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,6 +38,7 @@
             :event="day.dinner"
             eventDesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
           />
+          <button class="button is-info" @click="send">I like it! Send the itinerary to me!</button>
         </slide>
       </carousel>
     </div>
@@ -102,6 +103,12 @@ export default {
               console.log(err);
             });
         });
+    },
+    send: function() {
+      axios.post("http://localhost:3000/email", this.plan)
+      // .then(res => {
+      //   console.log(res);
+      // })
     }
   },
   filters: {
